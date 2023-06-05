2b3ef21fae5fa6026192c8af7be3deeb01983cd2
zibokapi <zibokapi@gmail.com>
2023-06-06 00:49:02 +0800

    cmon

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -351,8 +351,8 @@ class Tensor:
 
   @staticmethod
   def _tri(r:int, c:int, k:int=0) -> Tensor: return Tensor.arange(r).unsqueeze(1).expand(r,c) <= Tensor.arange(c-k, start=-k).unsqueeze(0).expand(r,c)
-  def triu(self:Tensor, k:int=0, inf_mask:bool=False) -> Tensor: return self._tri(*self.shape[-2:], k).where(self, Tensor.zeros_like(self)) if not inf_mask else self ** self._tri(*self.shape[-2:], k) - 1
-  def tril(self:Tensor, k:int=0, inf_mask:bool=False) -> Tensor: return self._tri(*self.shape[-2:], k=k+1).where(Tensor.zeros_like(self), self) if not inf_mask else self ** -(self._tri(*self.shape[-2:], k+1) - 1) - 1
+  def triu(self:Tensor, k:int=0, inf_mask:bool=False) -> Tensor: return self._tri(self.shape[-2:][0], self.shape[-2:][0], k).where(self, Tensor.zeros_like(self)) if not inf_mask else self ** self._tri(self.shape[-2:][0], self.shape[-2:][1], k) - 1
+  def tril(self:Tensor, k:int=0, inf_mask:bool=False) -> Tensor: return self._tri(self.shape[-2:][0], self.shape[-2:][1], k=k+1).where(Tensor.zeros_like(self), self) if not inf_mask else self ** -(self._tri(self.shape[-2:], self.shape[-2:][1], k+1) - 1) - 1
   # ***** reduce ops *****
 
   def _reduce(self, fxn:Type[Function], axis:Optional[Union[int, Tuple[int, ...]]]=None, keepdim=False):
