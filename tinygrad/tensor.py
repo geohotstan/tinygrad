eeec0cf2dcd33d976f6ac5f7d9d103c5113f1500
zibokapi <zibokapi@gmail.com>
2023-08-14 15:01:01 -0700

    asdf

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -564,7 +564,7 @@ class Tensor:
   def square(self): return self*self
   def clip(self, min_, max_): return self.maximum(min_).minimum(max_)
   def abs(self): return self.relu() + (-self).relu()
-  def sign(self): return self / (self.abs() + 1e-10)
+  def sign(self): return self / (self.abs() + 1e-5)
   def reciprocal(self): return 1.0/self
 
   # ***** activation functions (unary) *****
