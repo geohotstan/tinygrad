8052fb103eb9c49d8b2b5e2b7bab8319ec8f170e
zibokapi <zibokapi@gmail.com>
2024-03-28 22:49:11 +0800

    1234

--- tinygrad/renderer/assembly.py
+++ tinygrad/renderer/assembly.py
@@ -211,7 +211,10 @@ def uops_to_asm(lang:AssemblyLanguage, function_name:str, uops:UOpGraph) -> str:
         r[u] = f"%{args.expr}"
         if lang.load_global: kk(*lang.render_load(args.expr, ssa(u, 'dat', dtype=lang.types[dtype]), dtype, ss=".param"))
       elif uop is UOps.DEFINE_GLOBAL:
-        if len(bufs) < args[0]: continue
+        while len(bufs) < args[0]:
+          bufs.append((args[1], PtrDType(dtypes.float)))
+          r[u] = f"%{args[1]}"
+          kk(*lang.render_load(args[1], ssa(u, 'dat', dtype=lang.types[dtypes.ulong]), dtypes.ulong, ss=".param"))
         assert len(bufs) == args[0], f"missed a global buffer {len(bufs)} {args}"
         bufs.append((args[1], dtype))
         r[u] = f"%{args[1]}"
